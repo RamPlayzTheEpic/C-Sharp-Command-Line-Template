f57f5ea77ce761e4feb50cdb34ed63efe7b5c346
RamPlayzTheEpic <105378964+RamPlayzTheEpic@users.noreply.github.com>
2025-04-12 17:13:12 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿while (true)
+{
+    Console.Write("> ");
+    string UserInput = Console.ReadLine().ToLower();
+
+    if (UserInput == "cls")
+    {
+        Console.Clear();
+    }
+    else if (UserInput == "beep")
+    {
+        Console.Beep();
+    }
+    else if (UserInput == "help")
+    {
+        Console.WriteLine("help - bring this us\ncls - clear the console\nbeep - make a beep");
+    }
+    else
+    {
+        Console.WriteLine("\nThe command: \"" + UserInput + "\" doesn't exist. Please input \"help\" if u need some.");
+    }
+}
